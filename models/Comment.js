2bdbbf8e27782968c9f718d9fc768443b5adb1a9
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-03-06 13:31:36 -0500

    Refactor models and fix formatting

--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-
 // Create model for Comments
 class Comment extends Model {}
 Comment.init(
@@ -24,24 +23,23 @@ Comment.init(
       references: {
         model: 'user',
         key: 'id',
-      }
+      },
     },
     post_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'post',
         key: 'id',
-      }
-    }
+      },
+    },
   },
   {
     sequelize,
     timestamps: true,
     freezeTableName: true,
     underscored: true,
-    modelName: 'comment'
-  }
+    modelName: 'comment',
+  },
 );
 
 module.exports = Comment;
-
