0197881c3f7ec8b8dd6a8890cdecb90d9c45f1a7
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-03-05 20:44:50 -0500

    Fix syntax error in Like model

--- models/Like.js
+++ models/Like.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Like extends Model {}
+class Like extends Model {};
 
 Like.init(
     {
