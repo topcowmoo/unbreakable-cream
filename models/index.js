380dfbc9e0e31921337a6aaa3c97ab9aec2dc501
Joyce77777777 <CCATA9328@GMAIL.COM>
2024-03-05 21:24:37 -0500

    UC 104 add index.js to model

---
+++ models/index.js
@@ -0,0 +1,39 @@
+const User = require("./User");
+const Post = require("./post");
+const Favorites = require("./Favorites");
+const Like = require("./Like");
+const Comment = require("./Comment");
+
+
+// Build one-to-many relationship between User and Post.
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+// Build one-to-many relationship between User and Comment.
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+  });
+
+// Build one-to-many relationship between Post and Comment.
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+// Build one-to-many relationship between Post and Favorites.
+Post.hasMany(Favorites, {
+    foreignKey: "post_id",
+  });
+// Build one-to-many relationship between Post and Like.
+Post.hasMany(Like, {
+    foreignKey: "post_id",
+  });
+
+// Build one-to-many relationship between User and Like.
+User.hasMany(Like, {
+  foreignKey: "user_id",
+});
+// Build one-to-many relationship between User and Favorites.
+User.hasMany(Favorites, {
+    foreignKey: "user_id",
+  });
+
+module.exports = { User, Post, Favorites, Comment, Like };
